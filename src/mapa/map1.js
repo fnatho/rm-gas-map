d7293610bddcd01b4ed1d258ba9724856795b3b4
fnatho94 <fnatho@uc.cl>
2021-11-12 15:13:07 -0300

    Etapa 5

---
+++ src/mapa/map1.js
@@ -0,0 +1,92 @@
+var width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0),
+    height = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+var svg = d3.select("body")
+    .append("svg")
+    .style("cursor", "move");
+svg.attr("viewBox", "50 10 " + width + " " + height)
+    .attr("preserveAspectRatio", "xMinYMin");
+var zoom = d3.zoom()
+    .on("zoom", function () {
+        var transform = d3.zoomTransform(this);
+        map.attr("transform", transform);
+    });
+
+svg.call(zoom);
+var map = svg.append("g")
+    .attr("class", "map");
+
+d3.queue()
+    .defer(d3.json, "src/mapa/geo.json")
+    .defer(d3.json, "src/mapa/energia-rm.json")
+    .await(function (error, properties, data) {
+        if (error) {
+            console.error('Oh dear, something went wrong: ' + error);
+        }
+        else {
+            drawMap(properties, data);
+        }
+    });
+function drawMap(properties, data) {
+    // geoMercator projection
+    var projection = d3.geoMercator() //d3.geoOrthographic()
+        .scale(130)
+        .translate([width / 2, height / 1.5]);
+    // geoPath projection
+    var path = d3.geoPath().projection(projection);
+    //colors for population metrics
+    var color = d3.scaleThreshold()
+        .domain([750, 800, 850, 900, 950, 1000, 1050, 1100, 1150])
+        .range(["#f7fcfd", "#e0ecf4", "#bfd3e6", "#9ebcda", "#8c96c6", "#8c6bb1", "#88419d", "#810f7c", "#4d004b"]);
+    var features = topojson.feature(properties, properties.features).features;
+    var GasValue = {};
+
+    data.forEach(function (d) {
+        GasValue[d.nom_com] = {
+            bencina93 : d.bencina93,
+            bencina95 : d.bencina95,
+            bencina97 : d.bencina97,
+            diesel  : d.diesel,
+        }
+    });
+    features.forEach(function (d) {
+        d.details = GasValue[d.NOM_COM] ? GasValue[d.NOM_COM] : {};
+    });
+    map.append("g")
+        .selectAll("path")
+        .data(features)
+        .enter().append("path")
+        .attr("NOM_COM", function (d) {
+            return d.NOM_COM;
+        })
+        .attr("COD_COMUNA", function (d) {
+            return d.COD_COMUNA;
+        })
+        .attr("d", path)
+        .style("fill", function (d) {
+            return d.details && d.details.bencina93 ? color(d.details.bencina93) : undefined;
+        })
+        .on('mouseover', function (d) {
+            d3.select(this)
+                .style("stroke", "white")
+                .style("stroke-width", 1)
+                .style("cursor", "pointer");
+            d3.select(".NOM_COM")
+                .text(d.properties.NOM_COM);
+
+            d3.select(".bencina93")
+                .text(d.details && d.details.bencina93 && "bencina93 " + d.details.bencina93 || "¯\\_(ツ)_/¯");
+
+            d3.select(".bencina95")
+                .text(d.details && d.details.bencina95 && "bencina95" + d.details.bencina95 || "¯\\_(ツ)_/¯");
+
+            d3.select('.details')
+                .style('visibility', "visible")
+        })
+        .on('mouseout', function (d) {
+            d3.select(this)
+                .style("stroke", null)
+                .style("stroke-width", 0.25);
+            d3.select('.details')
+                .style('visibility', "hidden");
+        });
+}
--- src/mapa/vis.js
+++ src/mapa/vis.js
@@ -1,8 +1,6 @@
-let rmLink = 'node src/mapa/geo.json"'
-let gasLink = 'src/mapa/energia-rm.json"'
+let rmLink = './src/mapa/results.json"'
 
 let rmData
-let gasData
 
 
 let canvas = d3.select('#canvas')
@@ -12,25 +10,74 @@ let drawMap = () => {
 }
 console.log(d3.json(rmLink))
 
+
+//Fetch Data
 d3.json(rmLink).then(
     (data, error) => {
         if (error) {
-            console.log(log)
+            console.log(error)
         } else {
             rmData = data
+            console.log('RM Data')
             console.log(rmData)
 
-            d3.json(gasLink).then(
-                (data, error) => {
-                    if (error) {
-                        console.log(error)
-                    } else {
-                        gasData = data
-                        console.log(gasData)
-                    }
-                }
-            )
+
+            drawMap()
+
 
         }
     }
 )
+
+// Understanding the data we need
+/*
+countyData = data
+countyData = topojson.feature(countyData, countyData.objects.counties)
+console.log('County Data')
+console.log(countyData)
+
+countyData = topojson.feature(countyData, countyData.objects.counties).features
+
+// My choropleth should have counties with a corresponding class="county" that represent the data
+canvas.selectAll('path')
+        .data(countyData)
+        .enter()
+        .append('path')
+        .attr('d', d3.geoPath())
+        .attr('class', 'county')
+
+        // Fill Coloro
+
+        .attr('fill', (item) => {
+            let fips = item['id']
+            let county = educationData.find((county) => {
+                return county['fips'] === fips
+            })
+            let percentage = county['bachelorsOrHigher']
+            if (percentage <= 15){
+                return 'tomato'
+            }else if (percentage <= 30){
+                return 'orange'
+            } else if (percentage <= 45){
+                return 'lightgreen'
+            } else {
+                return 'limegreen'
+            }
+          })
+
+          //My counties should each have data-fips and data-education properties containing their corresponding fips and education values
+
+          .attr('data-fips', (item) => {
+            return item['id']
+        })
+        .attr('data-education', (item) => {
+            let fips = item['id']
+            let county = educationData.find((county) => {
+                return county['fips'] === fips
+            })
+            let percentage = county['bachelorsOrHigher']
+            return percentage
+        })
+
+*/
+
